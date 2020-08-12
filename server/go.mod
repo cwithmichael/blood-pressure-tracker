c9cd1866cc4bb8d9db61edc5f4f74b21720eb227
Michael Terrell <cwithmichael@gmail.com>
2020-08-12 08:48:26 -0500

    adding support for Redis

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,7 @@ module github.com/cwithmichael/blood-pressure-tracker
 go 1.14
 
 require (
+	github.com/gomodule/redigo v1.8.2
 	github.com/gorilla/mux v1.7.4
 	github.com/rs/cors v1.7.0
 )
--- server/main.go
+++ server/main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/gomodule/redigo/redis"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
@@ -20,16 +21,41 @@ type Reading struct {
 	ReadingDate int64 `json:"readingDate"`
 }
 
-var readings map[int]Reading
-var count int
+var redisConn redis.Conn
 
 func ReadingsHandler(w http.ResponseWriter, r *http.Request) {
-	var values []Reading
-	for _, v := range readings {
-		values = append(values, v)
+	var readings []Reading
+	keys, err := redis.Strings(redisConn.Do("SMEMBERS", "readings"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "failed to get readings"})
+		return
+	}
+	for _, key := range keys {
+		readingHash, err := redis.StringMap(redisConn.Do("HGETALL", key))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(map[string]string{"error": "failed to get readings"})
+			return
+		}
+		reading := Reading{}
+		for k, v := range readingHash {
+			switch k {
+			case "systolic":
+				reading.Systolic, err = strconv.Atoi(v)
+			case "diastolic":
+				reading.Diastolic, err = strconv.Atoi(v)
+			case "pulse":
+				reading.Pulse, err = strconv.Atoi(v)
+			case "reading-date":
+				date, _ := strconv.Atoi(v)
+				reading.ReadingDate = int64(date)
+			}
+		}
+		readings = append(readings, reading)
 	}
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(values)
+	json.NewEncoder(w).Encode(readings)
 }
 
 func ReadingsPostHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,32 +64,58 @@ func ReadingsPostHandler(w http.ResponseWriter, r *http.Request) {
 	var reading Reading
 	if err := decoder.Decode(&reading); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid request"})
 		return
 	}
 	reading.ReadingDate = time.Now().Unix()
+	count, err := redis.Int(redisConn.Do("GET", "readings-count"))
+	if err != nil {
+		_, err = redisConn.Do("SET", "readings-count", 0)
+	}
+
 	reading.ID = count
-	readings[count] = reading
-	count++
+	countStr := strconv.Itoa(count)
+	redisKey := "reading:" + countStr
+	_, err = redisConn.Do("HMSET", redisKey,
+		"systolic", reading.Systolic,
+		"diastolic", reading.Diastolic,
+		"pulse", reading.Pulse,
+		"reading-date", reading.ReadingDate)
+	_, err = redisConn.Do("SADD", "readings", redisKey)
+	_, err = redisConn.Do("INCR", "readings-count")
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(reading)
 }
 
 func ReadingsDeleteHandler(w http.ResponseWriter, r *http.Request) {
 	res := strings.Split(r.URL.Path, "/")
-	id, err := strconv.Atoi(res[2])
-	_, ok := readings[id]
-	if err != nil || !ok {
+	id := res[2]
+	exists, err := redis.Bool(redisConn.Do("HEXISTS", "reading:"+id, "sytolic"))
+	if err != nil || !exists {
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(map[string]string{"Message": "Invalid Request"})
+		return
 	} else {
-		delete(readings, id)
-		json.NewEncoder(w).Encode(id)
+		_, err := redisConn.Do("DEL", "reading:"+id)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
 
 func main() {
-	readings = make(map[int]Reading)
+	var err error
+	redisConn, err = redis.Dial("tcp", "redis:6379")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer redisConn.Close()
 	r := mux.NewRouter()
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"}, // All origins
